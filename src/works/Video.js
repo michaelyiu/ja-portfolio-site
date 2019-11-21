ee566cbb13ba358e2b4cee90dd758be6690a8e1e
unknown <onew1ng3d@gmail.com>
2019-11-21 12:29:04 -0500

    video page filled out a bit

--- src/works/Video.js
+++ src/works/Video.js
@@ -1,9 +1,19 @@
 import React from 'react'
 
-export const Video = () => {
+const Video = (props) => {
+	const { video } = props;
+	console.log(video);
 	return (
-		<div>
-			<h1>sweetups</h1>
+
+		<div className="video-intro">
+			<div className="image">
+				<div className="play-button btn"></div>
+				<a /*href="javascript:"*/ id="btn_play" className="btn"><img src={video.snippet.thumbnails.high.url} alt="play video" /></a>
+			</div>
+
+			<iframe src={`https://www.youtube.com/embed/${video.id.videoId}`} frameBorder="0"></iframe>
 		</div>
 	)
 }
+
+export default Video;
--- src/works/Work.js
+++ src/works/Work.js
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Video from './Video';
 
 const Work = () => {
+	const [videos, setVideos] = useState([]);
+
+	useEffect(() => {
+		const fetchVideos = async () => {
+			const response = await fetch('https://www.googleapis.com/youtube/v3/search?key=AIzaSyCOqJ8PLVyqiDmrZWIVDsOPAt25fmeloBw&channelId=UCUMHM23In1ItaANJgsiFHzA&part=snippet,id&order=date&maxResults=20');
+			const json = await response.json();
+			setVideos(json.items);
+		}
+		fetchVideos();
+	}, [])
 
 	return (
-		<div>
-			<h1>Works</h1>
-			{/* If Jonathan wants to keep a central YouTube hub so I can make an api call here for just his channel then this could be done here */}
+		<div className="flex-container">
+			{
+				videos.reduce(
+					(filtered, video) => {
+						if (!video.id.videoId) {
+							return filtered;
+						}
+						return filtered.concat(
+							<Video video={video} key={video.id.videoId} />
+						);
+					}
+					, [])
+			}
 		</div>
 	)
 }
